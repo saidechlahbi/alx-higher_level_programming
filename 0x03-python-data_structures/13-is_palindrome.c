5adfe1b16f2468c03b74b7c8d7b5521081c4235e
saidechlahbi <echlahbisaid@gmail.com>
2023-12-05 17:09:35 -0800

    update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,11 +10,11 @@ int _is_palindrome(listint_t **head)
 {
 	if (head == NULL || *head == NULL)
 		return (1);
-	return (aux_palind(head, *head));
+	return (_aux_palind(head, *head));
 }
 
 /**
- * aux_palind - funct to know if is palindrome
+ * _aux_palind - funct to know if is palindrome
  * @head: head list
  * @end: end list
  */
@@ -22,7 +22,7 @@ int _aux_palind(listint_t **head, listint_t *end)
 {
 	if (end == NULL)
 		return (1);
-	if (aux_palind(head, end->next) && (*head)->n == end->n)
+	if (_aux_palind(head, end->next) && (*head)->n == end->n)
 	{
 		*head = (*head)->next;
 		return (1);
