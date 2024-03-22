1406467d3508fe056735d72f7922ea0f7c9cc6db
saidechlahbi <echlahbisaid@gmail.com>
2024-03-22 14:42:03 -0700

    gg

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+
+#!/usr/bin/python3
+"""
+write a script that takes in arguments and displays all values in the 
+states table of hbtn_0e_0_usa where name matches the argument. 
+But this time, write one that is safe from MySQL injections!
+Usage: ./3-my_safe_filter_states.py <mysql username> <mysql password> \
+                        <database name> <state name searched>
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE ? ORDER BY id ASC", (argv[4],))
+    states = c.fetchall()
+    [print(state) for state in states]
+    
+
