95e3c806ebd58fb8f6def696eff046f13d73ebfe
saidechlahbi <echlahbisaid@gmail.com>
2024-03-22 14:47:03 -0700

    gg

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""
-Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
-"""
-import sys
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-  db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost", 
-                       passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-  c = db.cursor()
-  state = sys.argv[4]
-  c.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", (state,))
-  cities = c.fetchall()
-  [print(city) for city in cities]
-  c.close()
-  db.close()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
 
